c35386a47e6a5f900a6662f13032931a3212dfa7
aliasgari77 <ali.asgari77@gmail.com>
2022-03-11 13:13:58 +0330

    bug fixed

--- script.js
+++ script.js
@@ -1,104 +1,101 @@
 function setup() {
-    const allEpisodes = getAllEpisodes();
-    let inputTitle = document.querySelector("#input_title");
-  
-    let selectSearch = document.createElement("select");
-    inputTitle.appendChild(selectSearch);
-    selectSearch.name = "episode_search";
-    selectSearch.id = "episode_search";
-    
-  
-    allEpisodes.forEach((episode) => {  
-        let option = document.createElement('option');
-        option.setAttribute("value", episode.id); 
-        option.appendChild(document.createTextNode(
-            "S".concat(String(episode.season).padStart(2, 0)) +
-            "E".concat(String(episode.number).padStart(2, 0)) +
-            " - ".concat(episode.name)
-        ));
-        selectSearch.appendChild(option);
-    });
-        selectSearch.onchange = function(){
-          allEpisodes.find(function(episode) {
-                if(episode.id == selectSearch.value){
-                  window.open(episode.url);
-                  return true;
-                }
-                return false;
-              });
-        };
-  
-  
-   let input = document.createElement("input");
-   let results = document.createElement("div"); 
-   inputTitle.appendChild(input);
-   inputTitle.appendChild(results);
-   results.className = "results";
-   input.className = "input";
-   input.id = "input_id";
-  
-   makePageForEpisodes(allEpisodes);
-  
-   let updateInput = document.querySelector('#input_id');
-   updateInput.addEventListener('input', function() {
-    let searchLine = updateInput.value;     
-    let searchLineLowerCase = searchLine.toLowerCase();
-    let titleElement = document.querySelector('.results'); 
-  
-    let result = allEpisodes.filter( episode => ( 
-      episode.name.toLowerCase().includes(searchLineLowerCase) || 
-      episode.summary.toLowerCase().includes(searchLineLowerCase) 
-    ) 
-  );
-  
-  titleElement.innerText = "Episodes found " + result.length + " / " + allEpisodes.length;
-    
-  makePageForEpisodes(result);
-  });
-  }
-  
-  function makePageForEpisodes(episodeList) {
-    const rootElem = document.getElementById("root");
-    rootElem.textContent = "";
-    for(let i = 0; i < episodeList.length; i++){
-      console.log(episodeList[i].name);
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
+}
 
-      let divBlock = document.createElement("div");
-      rootElem.appendChild(divBlock);
-      divBlock.className = "block";
-      let divTitle = document.createElement("div");
-      divBlock.appendChild(divTitle);
-      divTitle.className = "div_title"; 
-      let div = document.createElement("div");
-      div.textContent = episodeList[i].name;
-      divTitle.appendChild(div);
-      div.className = "name";
-      let number = document.createElement("div");
-      number.textContent = "S".concat(String(episodeList[i].season).padStart(2, 0)) +
-          "E".concat(String(episodeList[i].number).padStart(2, 0));
-      divTitle.appendChild(number);
-      number.className = "number";
-      let divAdd = document.createElement("div");
-      divBlock.appendChild(divAdd);
-      let mediumImage = document.createElement("img");
-      mediumImage.src = episodeList[i].image.medium;
-      mediumImage.className = "image";
-      divBlock.appendChild(mediumImage);
-      let summary = document.createElement("div");
-      divBlock.appendChild(summary);
-      let par = document.createElement("p");
-      summary.appendChild(par);
-      par.textContent = episodeList[i].summary;
-      par.className = "summary";
-    
-      let link = document.createElement("a");
-      link.href = episodeList[i].url;
-    }
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+}
+
+window.onload = setup;
+
+function getEpisodeName(singleEpisode) {
+  return `${singleEpisode.name} - ${
+    singleEpisode.season < 10
+      ? "S0" + singleEpisode.season
+      : "S" + singleEpisode.season
+  }${
+    singleEpisode.season < 10
+      ? "E0" + singleEpisode.number
+      : "E" + singleEpisode.number
   }
-  window.onload = setup;
-  
-  
-  
-  
-  
-  
+`;
+}
+
+function displayEpisode(singleEpisode) {
+  let main = document.querySelector("main");
+  let article = document.createElement("article");
+
+  let h1 = document.createElement("h1");
+  h1.innerText = getEpisodeName(singleEpisode); //
+  let img = document.createElement("img");
+  img.src = singleEpisode.image.medium;
+
+  let div = document.createElement("div");
+  div.innerHTML = singleEpisode.summary; //
+  article.appendChild(h1);
+  article.appendChild(img);
+  article.appendChild(div);
+  main.appendChild(article);
+  article.className = "episode";
+}
+
+const allEpisodes = getAllEpisodes();
+allEpisodes.forEach((episode) => displayEpisode(episode));
+fillDropList(allEpisodes);
+
+searchField = document.querySelector("#search-field");
+searchField.addEventListener("keyup", searchEpisode);
+
+function ShowCount(resultCounter, allCounter) {
+  result = document.querySelector("#search-count");
+  result.innerText = `Displaying ${resultCounter}/${allCounter} episodes..`;
+}
+
+function searchEpisode(e) {
+  inputValue = searchField.value;
+  console.log(inputValue);
+  console.log(
+    allEpisodes.filter((episode) =>
+      episode.name.toLowerCase().includes(inputValue.toLowerCase())
+    )
+  );
+
+  let main = document.querySelector(".all-episodes");
+  main.innerHTML = "";
+  searchResult = allEpisodes.filter(
+    (episode) =>
+      episode.name.toLowerCase().includes(inputValue.toLowerCase()) ||
+      episode.summary.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
+  searchResult.forEach((filtered) => displayEpisode(filtered));
+  ShowCount(searchResult.length, allEpisodes.length);
+}
+
+function fillDropList(episodes) {
+  let episodeList = document.querySelector("#episodesDropList");
+  episodes.forEach((item) => {
+    let option = document.createElement("option");
+    option.text = getEpisodeName(item);
+    option.value = item.id;
+    episodeList.add(option);
+  });
+}
+
+document
+  .querySelector("#episodesDropList")
+  .addEventListener("change", selectSingleEpisode);
+
+function selectSingleEpisode(e) {
+  if (e.target.value == 0) {
+    allEpisodes.forEach((episode) => displayEpisode(episode));
+  } else {
+    let singleResult = allEpisodes.filter(
+      (episode) => episode.id == e.target.value
+    );
+    let main = document.querySelector(".all-episodes");
+    main.innerHTML = "";
+    displayEpisode(singleResult[0]);
+  }
+}
